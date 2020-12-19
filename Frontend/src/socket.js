a2a89b18037f0aaf094d2e23fa76548301029840
CodyNS <codynscontact@gmail.com>
2020-12-18 23:56:07 -0700

    configured for dev mode for now

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -4,10 +4,9 @@ export default class SocketManager {
     static instance = null;
     constructor() {
         // CHANGE ME 
-        // For AWS: http://[IP Address]/
+        // For AWS: http://[IP Address]/   <-- ex: http://3.129.16.155
         // For localhost: http://localhost:5000/
-        this.socket = SocketIOClient('http://3.129.16.155'); 
-    }                              // http://3.129.16.155
+        this.socket = SocketIOClient('http://localhost:5000/'); 
 
     getSocket = () => {
         return this.socket;
